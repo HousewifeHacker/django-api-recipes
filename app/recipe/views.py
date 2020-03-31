a2a588f57593bd08357a35f98d91a43718984e10
housewifehacker <housewifehacker@gmail.com>
2020-03-30 22:34:51 -0500

    API to list a user's tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Tag
+from .serializers import TagSerializer
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """List all tags"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
